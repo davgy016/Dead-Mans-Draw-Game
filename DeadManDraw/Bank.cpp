147635945685b07e67286d69a34908ea95ea6f17
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-10 19:34:33 +0930

    Add Bank class skeleton code

---
+++ DeadManDraw/Bank.cpp
@@ -0,0 +1,23 @@
+#include "Bank.h"
+
+Bank::Bank(Player* player)
+{
+}
+
+Bank::~Bank()
+{
+}
+
+Player* Bank::getPlayer() const
+{
+	return nullptr;
+}
+
+void Bank::addCardsFromPlayeArea(CardCollection& playAreaCards)
+{
+}
+
+int Bank::calculateScore()
+{
+	return 0;
+}
---
+++ DeadManDraw/Bank.h
@@ -0,0 +1,22 @@
+#ifndef BANK_H
+#define BANK_H
+#include "CardContainer.h"
+#include "Player.h"
+
+class Bank :
+    public CardContainer
+{
+protected:
+    Player* _player;
+
+public:
+
+    Bank(Player* player);
+    ~Bank();
+    Player* getPlayer() const;
+    void addCardsFromPlayeArea(CardCollection& playAreaCards);
+    int calculateScore();
+};
+
+
+#endif // BANK_H
