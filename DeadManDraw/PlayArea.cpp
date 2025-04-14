4203691423dfada8c0dd5ad2d05d56976eb536fd
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-14 11:13:21 +0930

    Add PlayArea class methods

--- DeadManDraw/PlayArea.cpp
+++ DeadManDraw/PlayArea.cpp
@@ -1,18 +1,47 @@
 #include "PlayArea.h"
+#include <iostream>
 
-PlayArea::PlayArea()
+PlayArea::PlayArea():
+	CardContainer<VectorContainer>()
 {
 }
 
 PlayArea::~PlayArea()
 {
+	cards.clear();
 }
 
-bool PlayArea::isBust()
+//add card into play area
+void PlayArea::addCard(Card* card)
 {
+	if (card != nullptr) {
+		cards.push_back(card);		
+	}
+}
+
+
+bool PlayArea::isBust(Card* newCard) const
+{
+	for (Card* existingCard : cards) {
+		if (existingCard->getType() == newCard->getType()) {
+			return true;
+		}
+	}
 	return false;
 }
 
+//void PlayArea::clear()
+//{
+//	cards.clear();
+//}
+
+
+//remove all cards from PlayArea when is busted and stored in target container.
 void PlayArea::moveAllCardsTo(VectorContainer& container)
 {
+	for (Card* card : cards) {
+		container.push_back(card);
+	}
+	cards.clear();
+	
 }
--- DeadManDraw/PlayArea.h
+++ DeadManDraw/PlayArea.h
@@ -2,14 +2,20 @@
 #define PLAYAREA_H
 
 #include "CardContainer.h"
-class PlayArea : public CardContainer< VectorContainer>
+class PlayArea : public CardContainer<VectorContainer>
 {
 public:
 	PlayArea();
 	~PlayArea();
 
-	bool isBust();
+	//add card into play area
+	void addCard(Card* card) override;
+	
+	bool isBust(Card* newCard) const;
 
+	//void clear() override;	
+
+	//remove all cards when is busted
 	void moveAllCardsTo(VectorContainer& container);
 };
 
