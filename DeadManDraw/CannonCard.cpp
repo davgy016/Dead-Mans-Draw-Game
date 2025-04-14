adaef2253b4b0fbf6a8b9fa83c90f24cdde68ada
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-14 18:46:33 +0930

    Add code for input validation and also make some adustments

--- DeadManDraw/CannonCard.cpp
+++ DeadManDraw/CannonCard.cpp
@@ -26,15 +26,15 @@ void CannonCard::play(Game& game, Player& player)
 
     Player* otherPlayer = game.getOtherPlayer();
     
-    if (otherPlayer->getBank()->size() == 0) {
+    if (otherPlayer->getBank()->size() == 0 ) {
         std::cout << "No cards to discard from " << otherPlayer->getName() << "'s Bank" << std::endl;
+        return;
     }
 
     //get another player bank cards
     MapContainer& bankCards = otherPlayer->getBank()->getCards();
 
-    std::cout << "Available suits to discard from " << otherPlayer->getName() << "'s Bank:" << std::endl;
-    std::vector<CardType> avalableSuits;
+    std::vector<CardType> availableSuits;
     int index = 1;
 
     //if each suit has cards, then gets card type with highest value and stores in new vector of availableSuits. Then display available suits for user to choose
@@ -42,20 +42,34 @@ void CannonCard::play(Game& game, Player& player)
     for (auto& pair : bankCards) {
         if (!pair.second.empty()) {
             CardType ct = pair.first;
-            avalableSuits.push_back(ct);
+            availableSuits.push_back(ct);
+            std::cout << "Available suits to discard from " << otherPlayer->getName() << "'s Bank:" << std::endl;
             std::cout << index <<"  " << pair.second.front()->str() << std::endl;
             index++;
         }
     }
+    // Check if there are any available suits to discard
+    if (availableSuits.empty()) {
+        std::cout << "No cards available to discard from " << otherPlayer->getName() << "'s Bank" << std::endl;
+        return;
+    }
+
     //user select suit to discard
     int choice = 0;
-    while (choice<0 || choice > avalableSuits.size()) {
-        std::cout << "Choose a suit to discard 1-"<< avalableSuits.size() << std::endl;
+    while (choice<1 || choice > availableSuits.size()) {
+        std::cout << "Choose a suit to discard 1-"<< availableSuits.size() << std::endl;
         std::cin >> choice;
+
+        if (std::cin.fail()) {
+            std::cin.clear();           
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            choice = 0;
+            std::cout << "Invalid input. Please enter a number." << std::endl;
+        }
     }
 
     //get chosen suit
-    CardType chosenType = avalableSuits[choice - 1];
+    CardType chosenType = availableSuits[choice - 1];
     //remove chosen suit card with highest value from player's bank and store in new card
     Card* cardForDiscardPile=otherPlayer->getBank()->removeCard(chosenType);
     //new card will be added into DiscardPile   
