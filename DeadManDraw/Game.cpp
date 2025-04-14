625c3ce5033f9289251c4815994f2bb0c3333716
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-14 11:40:12 +0930

    Add startGame and endGame methods in Game class

--- DeadManDraw/Game.cpp
+++ DeadManDraw/Game.cpp
@@ -48,12 +48,43 @@ void Game::initialiseGame()
 
 void Game::startGame()
 {
-	
+	std::cout << "Start the game" << std::endl;
+	std::cout << "Players: " << _players.at(0) <<" vs " << _players.at(1) << std::endl;
+
+	while (_currentTurn <= _maxTurns && _deck->size() > 0) {
+		printGameState();
+		handlePlayerTurn();
+		nextPlayer();
+		if (_currentPlayerIndex == 0) {
+			_currentRound++;
+		}
+		_currentTurn++;
+	}
+
+	endGame();
 }
 
 void Game::endGame()
 {
-	
+	std::cout << "\n=== GAME OVER ===" << std::endl;
+	std::cout << "Final Scores:" << std::endl;
+
+	for (Player* player : _players) {
+		std::cout << player->getName() << ": " << player->getScore() << " points" << std::endl;
+		player->printBank();
+	}
+
+	// Determine winner
+	if (_players[0]->getScore() > _players[1]->getScore()) {
+		std::cout << _players[0]->getName() << " wins!" << std::endl;
+	}
+	else if (_players[1]->getScore() > _players[0]->getScore()) {
+		std::cout << _players[1]->getName() << " wins!" << std::endl;
+	}
+	else {
+		std::cout << "It's a tie!" << std::endl;
+	}
+
 }
 
 Deck* Game::getDeck() const
